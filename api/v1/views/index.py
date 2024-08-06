3a226a353e06c157051acf8538cc622b656775fc
azzaomer <azzaomer70@gmail.com>
2024-08-07 01:22:14 +0300

    api rout

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,9 +12,26 @@ Routes:
 """
 from api.v1.views import app_views
 from flask import Response, jsonify
+from models import storage
+from models.engine.db_storage import classes
 
 
 @app_views.route('/status')
 def return_status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/status")
+def count_obj():
+    """
+    Retrieves the number of each objects by type
+    """
+    objects = {
+        "amenities": storage.count(classes["Amenity"]),
+        "cities": storage.count(classes["City"]),
+        "places": storage.count(classes["Place"]),
+        "reviews": storage.count(classes["Review"]),
+        "states": storage.count(classes["State"]),
+        "users": storage.count(classes["User"]),
+    }
+    return jsonify(objects)
