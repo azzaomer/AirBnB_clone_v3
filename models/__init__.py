0e9a43dd78be26ad66b934eb430e73db8fc07cdb
azzaomer <azzaomer70@gmail.com>
2024-08-27 15:40:44 +0300

    models

--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,19 @@ from os import getenv
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
+    from models.engine import db_storage
+    storage = db_storage.DBStorage()
+    """
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    """
+    
 else:
+    """
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+    """
+
+    from models.engine import file_storage
+    storage = file_storage.FileStorage()
 storage.reload()
